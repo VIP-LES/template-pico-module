a1fee68ce9783924d0cdefb2723d197db6f8ed05
Sebastien Martinez <sebmar.496@gmail.com>
2025-10-14 20:33:49 -0400

    create interface layer between cyphal and MCP driver

--- cmake/libcanard.cmake
+++ cmake/libcanard.cmake
@@ -1,23 +1,22 @@
 # libcanard.cmake
 # adapts OpenCyphal's libcanard to CMake
 
-set(LIBCANARD_C "${CMAKE_CURRENT_SOURCE_DIR}/external/libcanard/libcanard/canard.c")
-set(LIBCANARD_H "${CMAKE_CURRENT_SOURCE_DIR}/external/libcanard/libcanard/canard.h")
-set(CAVL2 "${CMAKE_CURRENT_SOURCE_DIR}/external/libcanard/lib/cavl2")
+# Always use PROJECT_SOURCE_DIR so paths are relative to the repo root.
+set(LIBCANARD_DIR ${PROJECT_SOURCE_DIR}/external/libcanard/libcanard)
+set(CAVL2_DIR ${PROJECT_SOURCE_DIR}/external/libcanard/lib/cavl2)
 
+set(LIBCANARD_C ${LIBCANARD_DIR}/canard.c)
+set(LIBCANARD_H ${LIBCANARD_DIR}/canard.h)
 
 add_library(cavl2 INTERFACE)
-target_include_directories(cavl2 INTERFACE ${CAVL2} )
+target_include_directories(cavl2 INTERFACE ${CAVL2_DIR})
 
-add_library( libcanard ${LIBCANARD_C} )
+# Build libcanard
+add_library(libcanard ${LIBCANARD_C})
+target_link_libraries(libcanard PUBLIC cavl2)
 
-target_link_libraries( libcanard cavl2 )
-
-target_include_directories(
-    libcanard
-    PUBLIC
-    ${CMAKE_CURRENT_SOURCE_DIR}/external/libcanard/libcanard
-)
+# Export its include directory so anyone linking to it can find canard.h
+target_include_directories(libcanard PUBLIC ${LIBCANARD_DIR})
 
 set_target_properties(libcanard PROPERTIES
     PUBLIC_HEADER "${LIBCANARD_H}"
