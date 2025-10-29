182f88dcb355cb1a363c16ffa923d67d049afe8d
Matthew Lyon <matthew@lyon.systems>
2025-10-29 05:47:02 -0400

    Minimal working example with heartbeats and hardcoded ids

--- src/main.c
+++ src/main.c
@@ -1,25 +1,45 @@
 #include "drivers/mcp251xfd.h"
 #include "drivers/mcp251xfd/debug.h"
+#include "drivers/canard.h"
 #include "config.h"
 #include "canard.h"
-#include "cyphal/cyphal_memory.h"
-#include "cyphal/cyphal_porting.h"
-#include "hardware/gpio.h"
-#include "hardware/spi.h"
 #include "pico/stdlib.h"
-#include <stdint.h>
-#include <stdio.h>
+#include "canbus.h"
 
-// A volatile flag to be set by the ISR
-volatile bool can_message_received = false;
+#include <uavcan/node/Heartbeat_1_0.h>
 
-// Interrupt Service Routine for CAN message reception
-void can_irq_handler(uint gpio, uint32_t events)
-{
+// Service interrupts and flags
+static int can_rx_irq_pin = -1;
+volatile bool can_rx_pending = false;
+void gpio_irq(uint gpio, uint32_t events) {
     if (gpio == PIN_CAN_INT && (events & GPIO_IRQ_EDGE_FALL)) {
-        can_message_received = true;
+        can_rx_pending = true;
     }
 }
+void init_can_rx_irq(uint pin) {
+    can_rx_irq_pin = pin;
+    gpio_init(pin);
+    gpio_set_dir(pin, GPIO_IN);
+    gpio_pull_up(pin);
+    gpio_set_irq_enabled_with_callback(PIN_CAN_INT, GPIO_IRQ_EDGE_FALL, true, gpio_irq);
+}
+
+static void on_heartbeat(const struct CanardRxTransfer *transfer, void *user_reference) {
+    (void) user_reference;
+
+    uavcan_node_Heartbeat_1_0 msg;
+    uint inout = transfer->payload.size;
+    if (uavcan_node_Heartbeat_1_0_deserialize_(&msg, transfer->payload.data, &inout) < 0) return;
+
+    printf("[HB] node=%u uptime=%u mode=%u health=%u\n",
+           transfer->metadata.remote_node_id,
+           (unsigned)msg.uptime,
+           msg.mode.value,
+           msg.health.value);
+}
+
+static uavcan_node_Health_1_0 current_health = {.value = uavcan_node_Health_1_0_NOMINAL};
+static uavcan_node_Mode_1_0 current_mode = {.value = uavcan_node_Mode_1_0_OPERATIONAL};
 
 int main(void)
 {
@@ -27,20 +47,14 @@ int main(void)
     while(!stdio_usb_connected())
         sleep_ms(100);
 
-    // Setup GPIO interrupt for receiving CAN messages
-    gpio_init(PIN_CAN_INT);
-    gpio_set_dir(PIN_CAN_INT, GPIO_IN);
-    gpio_pull_up(PIN_CAN_INT);
-    // The MCP251XFD INT pins are active low, so we trigger on a falling edge.
-    gpio_set_irq_enabled_with_callback(PIN_CAN_INT, GPIO_IRQ_EDGE_FALL, true, &can_irq_handler);
+    init_can_rx_irq(PIN_CAN_INT);
+    
 
     printf("Starting MCP2518FD test...\n");
-    MCP251XFD can;
-    eERRORRESULT result = initialize_CAN(&can_config, &can);
+    MCP251XFD candev;
+    eERRORRESULT result = initialize_CAN(&can_config, &candev);
     const char* reason = mcp251xfd_debug_error_reason(result);
-
     printf("Result of the initialization: %s\n", reason);
-
     if (result != ERR_OK) {
         printf("Halting due to CAN initialization failure.\n");
         while (true) {
@@ -48,113 +62,48 @@ int main(void)
         }
     }
 
-    mcp251xfd_debug_print_operation_mode(&can);
-
-    // =====================
-    // CYPHAL INITIALIZATION
-    // ---------------------
-
-    // Initialize o1heap
-    cyphal_memory_init();
-
-    // Attach CAN device to Cyphal
-    cyphal_port_init(&can);
-
-    // Create the CanardInstance for this Cyphal node, which
-    // encapsulates all the required functions and data structures
-    // for Cyphal to work.
-    CanardMemoryResource memory = cyphal_memory_resource();
-    CanardInstance ins = canardInit(memory);
+    // libcanard setup
+    canard_memory_init();
+    struct CanardMemoryResource memory = canard_memory_resource();
+    struct CanardInstance can = canardInit(memory);
+    // TEST NODE ID (subject to change)
+    can.node_id = CAN_ID_SENSOR_MODULE;
 
     // Initialize a TX queue for outgoing frames
     struct CanardTxQueue tx_queue = canardTxInit(32, CANARD_MTU_CAN_FD, memory);
 
-    // EXAMPLE NODE ID (subject to change)
-    ins.node_id = 42;
+    CanardSubscriptionCallback cb_heartbeat = {
+        .handler = on_heartbeat,
+        .user_reference = NULL
+    };
+    struct CanardRxSubscription sub_heartbeat;
+    int8_t sub_result = canardRxSubscribe(
+        &can, 
+        CanardTransferKindMessage, 
+        uavcan_node_Heartbeat_1_0_FIXED_PORT_ID_, 
+        uavcan_node_Heartbeat_1_0_EXTENT_BYTES_, 
+        CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC,
+        &sub_heartbeat
+    );
+    sub_heartbeat.user_reference = &cb_heartbeat;
 
-    // Subscription object
-    static CanardRxSubscription test_sub;
 
-    int32_t sub_result = canardRxSubscribe(
-        &ins,
-        CanardTransferKindMessage, // Type of transfer: message (broadcast)
-        1234, // KEEP OPPOSITE ON DIFFERENT BOARDS
-        CANARD_MTU_CAN_FD, // Correct extent (max payload size)
-        CANARD_DEFAULT_TRANSFER_ID_TIMEOUT_USEC, // Correct transfer timeout
-        &test_sub);
+    // Main Loop
+    absolute_time_t last_heartbeat = 0;
+    #define HEARTBEAT_INTERVAL_MS 1000
+    while (true) {
 
-    if (sub_result < 0) {
-        printf("Subscription failed: %ld\n", (long)sub_result);
-    }
-
-    // =====================
-
-    uint8_t message_counter = 0;
-
-    for (;;) {
-
-        // ====================
-        // SEND TESTING MESSAGE
-        // --------------------
-
-        // Example: simple payload of 3 bytes
-        uint8_t payload[4] = { message_counter, 0xAA, 0xBB, 0xCC };
-
-        // Build metadata describing this transfer
-        struct CanardTransferMetadata metadata = {
-            .priority = CanardPriorityNominal,
-            .transfer_kind = CanardTransferKindMessage,
-            .port_id = 2468, // KEEP OPPOSITE ON DIFFERENT BOARDS
-            .remote_node_id = CANARD_NODE_ID_UNSET, // broadcast
-            .transfer_id = message_counter++ // must increment per subject
-        };
-
-        // Wrap payload into a CanardPayload
-        struct CanardPayload pl = {
-            .size = sizeof(payload),
-            .data = payload
-        };
-
-        // Push transfer into TX queue
-        uint64_t frames_expired = 0;
-        int32_t result = canardTxPush(&tx_queue, &ins,
-            cyphal_port_get_usec() + 1000000, // 1-second deadline
-            &metadata, pl, 0, &frames_expired);
-
-        if (result < 0) {
-            printf("canardTxPush failed: %ld\n", (long)result);
-        } else {
-            printf("Pushed message with counter: %d\n", message_counter);
+        if (can_rx_pending) {
+            // read messages from can and call handlers for subscriptions
+            canard_mcp_rx_process(&candev, &can);
+            can_rx_pending = false;
         }
-
-        // Send the payload on the bus using the "cyphal_porting" layer
-        struct CanardTxQueueItem* item = canardTxPeek(&tx_queue);
-        while (item != NULL) {
-            cyphal_tx(&item->frame); // your hardware send
-            canardTxPop(&tx_queue, item);
-            canardTxFree(&tx_queue, &ins, item); // new mandatory free call
-            item = canardTxPeek(&tx_queue);
+        // Pull frames from the queue and send to our interface driver
+        int pollin_result = canardTxPoll(&tx_queue, &can, to_us_since_boot(get_absolute_time()), &candev, canard_mcp_tx_frame, NULL, NULL);
+        // Should check this output maybe?
+        if (get_absolute_time() > last_heartbeat) {
+            publish_heartbeat(&tx_queue, &can, current_health, current_mode);
+            last_heartbeat = make_timeout_time_ms(HEARTBEAT_INTERVAL_MS);
         }
-
-        // ====================
-
-        // ==========================
-        // CHECK FOR RECEIVED MESSAGE
-        // --------------------------
-
-        // Check if the interrupt has sent the "message received" flag;
-        // if so, call the "rx_process" from the "cyphal_porting" layer
-        // to receieve the message from the MCP receieve FIFO.
-        if (can_message_received || message_counter % 5 == 0) {
-            can_message_received = false;
-            printf("\n--- RX interrupt triggered ---\n");
-            cyphal_rx_process(&ins);
-            printf("--- RX drain complete ---\n\n");
-        }
-
-        // ==========================
-
-        // Delay between loops
-        sleep_ms(500);
     }
 }
