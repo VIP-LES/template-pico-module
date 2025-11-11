c02e2142ad16d2e0ee961034cd3ed46b265681a9
Matthew Lyon <matthew@lyon.systems>
2025-11-11 18:08:46 -0500

    1.0 Template module

--- src/main.c
+++ src/main.c
@@ -1,29 +1,33 @@
-#include "canard.h"
-#include "canbus.h"
+#include "leos/log.h"
 #include "config.h"
-#include "errors.h"
-#include "log.h"
-
+#include "leos/mcp251xfd.h"
+#include "leos/cyphal/node.h"
+#include "module_setup.h"
 #include "pico/stdlib.h"
 
-#include <uavcan/node/Mode_1_0.h>
-
-int main() {
+void main() {
     // --- INITIALIZE MODULE ---
-    stdio_init_all();
+    leos_log_init_console(ULOG_INFO_LEVEL);
+    MCP251XFD dev;
+    leos_cyphal_node_t node;
+    if (init_module(&dev, &node) < 0) {
+        LOG_ERROR("A critical communications error has occured. This node is offline.");
+        return;
+    }
 
-    // Sleep until USB is connected
-    while (!stdio_usb_connected())
-        sleep_ms(100);
-    LOG_INFO("USB STDIO Connected. Starting application.");
+    // Your setup code goes here
+
 
     // After finishing initialization, set our mode to operational
-    canbus_set_node_mode(uavcan_node_Mode_1_0_OPERATIONAL);
+    node.mode.value = uavcan_node_Mode_1_0_OPERATIONAL;
+    // Turn on board LED to indicate setup success.
+    gpio_put(PICO_DEFAULT_LED_PIN, 1);
 
     // --- MAIN LOOP ---
     LOG_INFO("Entering main loop...");
     while (true) {
-        canbus_task(); // Must run as often as possible
+        leos_mcp251xfd_task(&dev);
+        leos_cyphal_task(&node);
 
         // Your looping code goes here
     }
